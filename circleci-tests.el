2061c1e0674cf97df44ab2af537d9f93b1ae8ead
Phil Hagelberg <phil@hagelb.org>
2017-05-12 16:04:36 -0700

    Actual test.

--- circleci-tests.el
+++ circleci-tests.el
@@ -1,10 +1,15 @@
 (ert-deftest run-on-self ()
-  (should (equal 2 (+ 1 1)))
-  ;; (cl-letf ((auth-source-search (&rest _)
-  ;;                               `((:secret ,(getenv "CIRCLECI_TOKEN")
-  ;;                                          :save-function ignore))))
-  ;;   ;; prefix arg set to 1 means infer project/branch
-  ;;   (circleci 1))
-  )
-
-
+  (cl-letf (((symbol-function 'auth-source-search)
+             (lambda (&rest _)
+               `((:secret ,(apply-partially 'getenv "CIRCLECI_TOKEN")
+                          :save-function ignore)))))
+    (circleci 1) ; prefix arg set to 1 means infer project/branch
+    (sit-for 4)
+    (with-current-buffer (format "*circleci-output: %s*" circleci-last-project)
+      (goto-char (point-min))
+      (search-forward "ert-run-tests-batch-and-exit")
+      (backward-char)
+      (push-button)
+      (sit-for 4)
+      (goto-char (point-min))
+      (search-forward "run-on-self"))))
